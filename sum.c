3a60eadaf6d00bc12db6b2d853475c8a5c3a1135
Trevor <trevor.k.gachagua@gmail.com>
2024-06-14 13:10:00 +0300

    first commit

---
+++ sum.c
@@ -0,0 +1,12 @@
+//sum of two numbers
+#include<stdio.h>  
+int main(){
+    //adding to numbers
+    
+    int a = 10;
+    int b = 20;
+    int sum;
+    sum = a+b;
+    printf("the sum of a and b is %d", sum);
+    return 0;
+} 

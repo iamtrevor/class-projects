24d5562bb7a2c3455b150a1723d080bf9a12c2cb
Trevor <trevor.k.gachagua@gmail.com>
2024-06-22 15:36:04 +0300

    first commit

--- pay_rate.c
+++ pay_rate.c
@@ -1,3 +1,5 @@
+// program to calculate pay rate
+
 #include <stdio.h>
 int main() {
     int hours = 6;
@@ -6,12 +8,12 @@ int main() {
     
 
     if(hours < 40){
-       printf("The total price is %d", total_pay);
+       printf("The total pay is %d", total_pay);
     }
 
     else{
         total_pay = 1.5 * total_pay;
-        printf("The total price is %d", total_pay);
+        printf("The total pay is %d", total_pay);
     }
 
 
